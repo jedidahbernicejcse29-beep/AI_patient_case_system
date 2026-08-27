cb400e041dd36a1d7e490f3ed5e623f07f6f48ce
jedidahbernicejcse29-beep <jedidahbernice.j.cse29@psvpec.in>
2026-08-27 13:34:27 +0530

    Update routes.py

--- backend/routes.py
+++ backend/routes.py
@@ -1,1 +1,14 @@
+from flask import Blueprint, request, jsonify
 
+patient_routes = Blueprint("patient_routes", __name__)
+
+
+@patient_routes.route("/patient", methods=["POST"])
+def add_patient():
+
+    data = request.get_json()
+
+    return jsonify({
+        "message": "Patient added successfully",
+        "data": data
+    })
