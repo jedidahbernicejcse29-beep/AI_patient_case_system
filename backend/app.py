c204606da52e2b6675a70ccfee5b175341c52baf
jedidahbernicejcse29-beep <jedidahbernice.j.cse29@psvpec.in>
2026-08-27 13:32:00 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,34 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "MediKiosk Backend is running"
+    })
+
+
+@app.route("/patient", methods=["POST"])
+def patient():
+    data = request.get_json()
+
+    name = data.get("name")
+    age = data.get("age")
+    gender = data.get("gender")
+    symptoms = data.get("symptoms")
+
+    return jsonify({
+        "message": "Patient information received",
+        "patient": {
+            "name": name,
+            "age": age,
+            "gender": gender,
+            "symptoms": symptoms
+        }
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
