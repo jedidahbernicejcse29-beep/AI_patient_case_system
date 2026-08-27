7bab0f4f7fc6f68cdbab8d5f2a3dc8455b91b797
jedidahbernicejcse29-beep <jedidahbernice.j.cse29@psvpec.in>
2026-08-27 13:33:03 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,6 @@
-
+class Patient:
+    def __init__(self, name, age, gender, symptoms):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.symptoms = symptoms
